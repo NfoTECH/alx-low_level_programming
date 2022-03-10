8dd31443bc757bb47bcbd94499b2d188ac6ddba6
NfoTECH <fortunenwachukwu@gmail.com>
2022-03-10 18:20:57 +0100

    My first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ *main - Is the function name and start of execution
+ *return: return 0 shows successful execution of the main() function
+ */
+
+int main(void)
+{
+    puts("\"Progammimg is like building a multilingual puzzle");
+    return 0;
+}
