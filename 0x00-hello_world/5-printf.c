ad4f494503edf40ca945e56455be1fdeff2bc94d
NfoTECH <fortunenwachukwu@gmail.com>
2022-03-10 20:56:24 +0100

    Using printf in C

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main - This is where the execution starts
+ *Return: return 0 shows successful execution
+ */
+
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
