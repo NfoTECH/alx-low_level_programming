7694b3fe6575b41418a646c0c47a86ee4eaf423e
NfoTECH <fortunenwachukwu@gmail.com>
2022-03-11 17:22:07 +0100

    Print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Execute base 10 no
+ *
+ * Return: return 0 as success
+ **/
+int main(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+
+	putchar('\num');
+
+	return (0);
+}
