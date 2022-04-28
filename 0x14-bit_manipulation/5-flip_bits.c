9db53145c46788c345d77d2ff1f6b0ec7bc0697c
NfoTECH <fortunenwachukwu@gmail.com>
2022-04-28 22:14:54 +0100

    returns the number of bits you would need to flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * clear_bit - sets value of a bit to 0
+ * clear_bit - sets the value of a bit to 0.
  * at a given index.
  * @n: pointer of an unsigned long int.
  * @index: index of the bit.
  *
- * Return: 1 for success, -1 for unsuccessful.
+ * Return: 1 if it worked, -1 if it didn't.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
